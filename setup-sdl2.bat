5f83a3bda7126d31296cf10b98072ebcaf8ecb1d
Davilarek <62715937+Davilarek@users.noreply.github.com>
2023-04-11 17:42:51 +0200

    Update setup-sdl2.bat
    Guh I really forgot to uncomment

--- setup-sdl2.bat
+++ setup-sdl2.bat
@@ -1,11 +1,8 @@
 @echo off
-@REM call download-sdl2.bat
-@REM tar -xvzf *.tar.gz -C .\resources\SDL
+call download-sdl2.bat
 mkdir resources\SDL
 for /r %%i in (*.tar.gz) do tar -xvzf "%%i" -C resources\SDL
 cd resources\SDL
-@REM for %%i in (*) do move %%i\*.* .\
-@REM for /r %%i in (*.*) do echo %%i
 for /d %%i in (".\*") do (
     xcopy /Y /s /e "%%i\*" ".\"
     rd /s /q "%%i"
